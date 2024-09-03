9c73087f776beb57edc9c1c157921e799ccb6f6f
Wallace Gabriel <gs008963@gmail.com>
2024-09-02 22:44:32 -0300

    quarto

---
+++ quarto.py
@@ -0,0 +1,19 @@
+sp = 67836.43
+rj = 36678.66
+mg = 29229.88
+es = 27165.48
+outros = 19849.53
+
+total = sp + rj + mg + es + outros
+
+sp_porcentagem = (sp / total) * 100
+rj_porcentagem = (rj / total) * 100
+mg_porcentagem = (mg / total) * 100
+es_porcentagem = (es / total) * 100
+outros_porcentagem = (outros / total) * 100
+
+print(f"SP: {sp_porcentagem:.2f}%")
+print(f"RJ: {rj_porcentagem:.2f}%")
+print(f"MG: {mg_porcentagem:.2f}%")
+print(f"ES: {es_porcentagem:.2f}%")
+print(f"Outros: {outros_porcentagem:.2f}%")
